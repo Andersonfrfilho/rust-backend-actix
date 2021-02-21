7b47858d50170488b2129ee0dd92b66e37406f9d
Anderson Fernandes <miyazaki@kali.kaliHpElite820g3>
2021-02-21 12:02:42 -0300

    feat: adjuste middleware

--- src/middlewares/auth.rs
+++ src/middlewares/auth.rs
@@ -1,14 +1,37 @@
-use actix_web::{dev::ServiceRequest,dev::HttpResponseBuilder,error, get, http::header, http::StatusCode, App, HttpResponse};
-use jsonwebtoken::errors::ErrorKind;
-use actix_web_httpauth::{
-  extractors::bearer::BearerAuth,
+use actix_web::{
+    dev::HttpResponseBuilder, dev::ServiceRequest, error, get, http::header, http::StatusCode, App,
+    Error, HttpResponse,
 };
-use serde::{Serialize,Deserialize};
-use jsonwebtoken::{decode, Validation, DecodingKey};
-
+use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
+use actix_web_httpauth::extractors::AuthenticationError;
 use derive_more::{Display, Error};
+use jsonwebtoken::errors::ErrorKind;
+use jsonwebtoken::{decode, DecodingKey, Validation};
+use serde::{Deserialize, Serialize};
 
+pub async fn validator(
+    req: ServiceRequest,
+    credentials: BearerAuth,
+) -> Result<ServiceRequest, Error> {
+    let key = b"secret";
+    let decoded = decode::<Claims>(
+        &credentials.token(),
+        &DecodingKey::from_secret(key),
+        &Validation::default(),
+    );
 
+    if !decoded.is_err() {
+        Ok(req)
+    } else {
+        let config = req
+            .app_data::<Config>()
+            .map(|data| data.clone())
+            .unwrap_or_else(Default::default)
+            .scope("urn:example:channel=HBO&urn:example:rating=G,PG-13");
+
+        Err(AuthenticationError::from(config).into())
+    }
+}
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
     sub: String,
@@ -16,23 +39,23 @@ struct Claims {
     exp: usize,
 }
 
-pub async fn validator(
-  req: ServiceRequest,
-  credentials: BearerAuth,
-) -> Result<ServiceRequest, actix_web::Error> {
-  let key = b"secret";
-  let token = credentials.token();
-  let token_data = match decode::<Claims>(
-    &token,
-    &DecodingKey::from_secret(key),
-    &Validation::default(),
-  ) {
-    Ok(c) => c,
-    Err(err) => match *err.kind() {
-        ErrorKind::InvalidToken => panic!(err), // Example on how to handle a specific error
-        _ => panic!(err),
-    },
-  };
-  println!("{:?}",token_data);
-  Ok(req)
-}
+// pub async fn validator(
+//     req: ServiceRequest,
+//     credentials: BearerAuth,
+// ) -> Result<ServiceRequest, actix_web::Error> {
+//     let key = b"secret";
+//     let token = credentials.token();
+//     let token_data = match decode::<Claims>(
+//         &token,
+//         &DecodingKey::from_secret(key),
+//         &Validation::default(),
+//     ) {
+//         Ok(c) => c,
+//         Err(err) => match *err.kind() {
+//             ErrorKind::InvalidToken => panic!(err), // Example on how to handle a specific error
+//             _ => panic!(err),
+//         },
+//     };
+//     println!("{:?}", token_data);
+//     Ok(req)
+// }
